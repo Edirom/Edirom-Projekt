f6a30da77e305ccb5d114be4f14656072fd4b2a2
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2016-06-01 10:36:06 +0200

    Homepage, CSS, Javascript and images

---
+++ typo3conf/ext/air_filemanager/pi1/air_filemanager.js
@@ -0,0 +1,22 @@
+﻿jQuery(document).ready(function() {
+
+	var fieldcount = jQuery('.fields').size() -1;
+	
+	jQuery('#tx_airfilemanager_addfields_btn').show();
+	
+	jQuery('#tx_airfilemanager_addfields_btn').click(function() {
+		
+		fieldcount++;
+		
+		var clonedElement = jQuery('.fields_template').clone();
+						
+		clonedElement
+			.html(clonedElement.html().replace(/###FIELD_NO###/g, fieldcount))
+			.html(clonedElement.html().replace(/###FILE_NUMBER###/g, fieldcount+1))
+			.attr('class', 'fields')
+			.show()
+			.insertBefore(jQuery('#insert_new_fields_here'));
+		
+		return false;
+	});
+});
